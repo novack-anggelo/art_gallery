8df6c7eb66229fd2594ddf11e8d83e56b1016c70
novack-anggelo <anggelo.novack@gmail.com>
2025-08-04 15:38:43 +0200

    dependencies setup - Hilt - Retrofit - Moshi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt.gradle) // Added for Hilt
+    alias(libs.plugins.ksp)         // Added for KSP
 }
 
 android {
@@ -50,6 +52,15 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
+
+    // Hilt
+    implementation(libs.hilt.android) // Added Hilt dependency
+    ksp(libs.hilt.compiler)          // Added Hilt compiler with KSP
+
+    // Retrofit & Moshi
+    implementation(libs.retrofit)                // Added Retrofit dependency
+    implementation(libs.retrofit.converter.moshi) // Added Moshi converter for Retrofit
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
