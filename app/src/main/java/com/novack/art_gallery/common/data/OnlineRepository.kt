9cb73fd0b782a5f5262c03a37a4da8c1ac695efa
novack-anggelo <anggelo.novack@gmail.com>
2025-08-05 18:03:55 +0200

    Added unit tests OnlineRepositoryTest.kt

--- app/src/main/java/com/novack/art_gallery/common/data/OnlineRepository.kt
+++ app/src/main/java/com/novack/art_gallery/common/data/OnlineRepository.kt
@@ -2,14 +2,16 @@ package com.novack.art_gallery.common.data
 
 import com.novack.art_gallery.common.data.mappers.toDomain
 import com.novack.art_gallery.common.data.retrofit.RetrofitNetwork
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class OnlineRepository @Inject constructor(
-    private val api: RetrofitNetwork
+    private val api: RetrofitNetwork,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : ArtRepository {
-    override suspend fun getArtworksOverview() = withContext(Dispatchers.IO) {
+    override suspend fun getArtworksOverview() = withContext(dispatcher) {
         api.getArtworksOverview().toDomain()
     }
 
