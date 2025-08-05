2f8be12a52b838601b04204bea8f128221126773
novack-anggelo <anggelo.novack@gmail.com>
2025-08-05 12:59:47 +0200

    Implemented Implemented UI for ArtOverviewScreen

--- app/src/main/java/com/novack/art_gallery/art_overview/ui/components/OverviewItem.kt
+++ app/src/main/java/com/novack/art_gallery/art_overview/ui/components/OverviewItem.kt
@@ -22,21 +22,19 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.novack.art_gallery.R
+import com.novack.art_gallery.art_overview.ui.ArtOverviewItemParam
 import com.novack.art_gallery.ui.common.IconSize
 import com.novack.art_gallery.ui.common.Padding
 import com.novack.art_gallery.ui.theme.Art_galleryTheme
 
 @Composable
 fun OverviewItem(
-    id: String,
-    artist: String,
-    title: String,
-    thumbnailUrl: String,
+    param: ArtOverviewItemParam,
     onClick: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
     Card(
-        onClick = { onClick(id) },
+        onClick = { onClick(param.id) },
         modifier = modifier.fillMaxWidth(),
         shape = MaterialTheme.shapes.medium,
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
@@ -50,12 +48,12 @@ fun OverviewItem(
             Row(verticalAlignment = Alignment.Bottom) {
                 AsyncImage(
                     model = ImageRequest.Builder(LocalContext.current)
-                        .data(thumbnailUrl)
+                        .data(param.thumbnailUrl)
                         .crossfade(true) // Smooth fade-in animation
                         .build(),
                     placeholder = painterResource(R.drawable.ic_art_placeholder),
                     error = painterResource(R.drawable.ic_art_error),
-                    contentDescription = "Thumbnail for art piece $title",
+                    contentDescription = "Thumbnail for art piece ${param.title}",
                     contentScale = ContentScale.Crop, // Crop to fill the bounds
                     modifier = Modifier
                         .size(IconSize.Thumbnail),
@@ -63,11 +61,11 @@ fun OverviewItem(
                         println("error Loading image: ${it.result.throwable}")
                     },
                     onLoading = {
-                        println("Loading thumbnail for art: $id")
+                        println("Loading thumbnail for art: ${param.id}")
                     }
                 )
                 Text(
-                    text = title,
+                    text = param.title,
                     style = MaterialTheme.typography.bodyLarge,
                     modifier = Modifier
                         .weight(1f)
@@ -78,7 +76,7 @@ fun OverviewItem(
             }
             Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.CenterEnd) {
                 Text(
-                    text = artist,
+                    text = param.artist,
                     style = MaterialTheme.typography.bodySmall,
                     modifier = Modifier.padding(top = Padding.SizeXXXXS)
                 )
@@ -93,10 +91,12 @@ fun OverviewItem(
 private fun OverviewItemPreview() {
     Art_galleryTheme {
         OverviewItem(
-            id = "1",
-            artist = "Artist",
-            title = "Title",
-            thumbnailUrl = "https://www.artic.edu/iiif/2/2d484387-2509-5e8e-2c43-22f9981972eb/full/843,/0/default.jpg",
+            param = ArtOverviewItemParam(
+                id = "1",
+                artist = "Artist",
+                title = "Title",
+                thumbnailUrl = "https://www.artic.edu/iiif/2/2d484387-2509-5e8e-2c43-22f9981972eb/full/843,/0/default.jpg",
+            ),
             onClick = {}
         )
     }
