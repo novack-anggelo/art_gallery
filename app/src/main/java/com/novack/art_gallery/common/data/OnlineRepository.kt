b751aeda1a6a7e19236d23fdf43347efc34f092c
novack-anggelo <anggelo.novack@gmail.com>
2025-08-05 15:38:00 +0200

    Implemented DI for data

--- app/src/main/java/com/novack/art_gallery/common/data/OnlineRepository.kt
+++ app/src/main/java/com/novack/art_gallery/common/data/OnlineRepository.kt
@@ -4,8 +4,9 @@ import com.novack.art_gallery.common.data.mappers.toDomain
 import com.novack.art_gallery.common.data.retrofit.RetrofitNetwork
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class OnlineRepository constructor(
+class OnlineRepository @Inject constructor(
     private val api: RetrofitNetwork
 ) : ArtRepository {
     override suspend fun getArtworksOverview() = withContext(Dispatchers.IO) {
